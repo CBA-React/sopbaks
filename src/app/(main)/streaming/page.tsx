13bf618f6340606f653b61eda562622e6d3d204d
Artem Zubkov <zubkov.artem46@gmail.com>
2025-09-21 15:44:12 +0300

    added streaming tabs with corresponding items

--- src/app/(main)/streaming/page.tsx
+++ src/app/(main)/streaming/page.tsx
@@ -2,11 +2,13 @@ import { JSX } from 'react';
 
 import RecentlyWatchedBlock from '@/shared/components/RecentlyWatched/RecentlyWatchedBlock';
 import StreamingBanner from '@/shared/components/StreamingBanner/StreamingBanner';
+import StreamingItemsBlock from '@/shared/components/StreamingItemsBlock/StreamingItemsBlock';
 
 export default function StreamingPage(): JSX.Element {
     return (
         <main>
             <StreamingBanner />
+            <StreamingItemsBlock />
             <RecentlyWatchedBlock />
         </main>
     );

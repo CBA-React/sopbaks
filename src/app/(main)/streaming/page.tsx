19b5510d4a416959592041a4777abbc95b91ca86
Artem Zubkov <zubkov.artem46@gmail.com>
2025-09-21 15:32:36 +0300

    added recently watched block

--- src/app/(main)/streaming/page.tsx
+++ src/app/(main)/streaming/page.tsx
@@ -1,11 +1,13 @@
 import { JSX } from 'react';
 
 import StreamingBanner from '@/shared/components/StreamingBanner/StreamingBanner';
+import RecentlyWatchedBlock from '@/shared/components/RecentlyWatched/RecentlyWatchedBlock';
 
 export default function StreamingPage(): JSX.Element {
     return (
         <main>
             <StreamingBanner />
+            <RecentlyWatchedBlock />
         </main>
     );
 }

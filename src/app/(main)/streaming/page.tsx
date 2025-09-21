3b6a99d52898d551f8fb5b5fe15b3c96085b5433
Artem Zubkov <zubkov.artem46@gmail.com>
2025-09-21 14:55:40 +0300

    added streaming banner at the top of the page

---
+++ src/app/(main)/streaming/page.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+import StreamingBanner from '@/shared/components/StreamingBanner/StreamingBanner';
+
+export default function StreamingPage(): JSX.Element {
+    return (
+        <main>
+            <StreamingBanner />
+        </main>
+    );
+}
